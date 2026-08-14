eecc36dea4adbc67e4d78c56bd7357618d0cbb5b
kk485 <870246263@qq.com>
2026-08-14 21:41:38 +0800

    添加统计后台

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -8,15 +8,15 @@ export const analyticsConfig: AnalyticsConfig = {
 	// Umami 统计配置
 	umamiAnalytics: {
 		// Umami Website ID
-		websiteId: "",
+		websiteId: "18eb154b-65b1-4f1a-8496-92fbdb769e51",
 		// Umami JS地址，支持使用自建
-		scriptUrl: "https://cloud.umami.is/script.js",
+		scriptUrl: "https://uma.baiym.top/script.js",
 		// Umami 会话回放脚本地址，支持使用自建
-		replaysScriptUrl: "https://cloud.umami.is/recorder.js",
+		replaysScriptUrl: "https://uma.baiym.top/recorder.js",
 		// 是否追踪出站链接
 		trackOutboundLinks: true,
 		// 是否收集浏览器性能指标
-		collectWebVitals: false,
+		collectWebVitals: true,
 		// 会话回放配置
 		replays: {
 			// 是否启用会话回放
